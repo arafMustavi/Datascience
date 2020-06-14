f081433d8dcf257e55ac22bc26d32050852a008c
Araf Mustavi <35128639+arafMustavi@users.noreply.github.com>
2020-06-15 03:30:16 +0600

    data cleaning ongoing

--- momadataset.py
+++ momadataset.py
@@ -1,10 +1,30 @@
+def clean_and_convert(date):
+    # check that we don't have an empty string
+    if date != "":
+        # move the rest of the function inside
+        # the if statement
+        date = date.replace("(", "")
+        date = date.replace(")", "")
+        date = int(date)
+    return date
+
 def replace_paranthesis(word):
     word = word.replace("(" , "")
     word = word.replace(")" , "")
     return word
 
-#### Removes the () from the strings
 
 for entry in moma:
-    entry[2] = replace_paranthesis(entry[2])
-    entry[5] = replace_paranthesis(entry[5])
+    entry[3] = replace_paranthesis(entry[2])
+    entry[4] = replace_paranthesis(entry[5])
+
+    entry[5] = entry[5].title()
+    if entry[5] == "":
+        entry[5] = "Gender Unknown/Other"
+    
+    entry[2] = entry[2].title()
+    if entry[2] == "":
+        entry[2] = "Nationality Unknown"
+        
+    entry[3] = clean_and_convert(entry[3])
+    entry[4] = clean_and_convert(entry[4])
