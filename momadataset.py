5c2a5ab53b749f291d8940cc92e4f03ce31eca54
Araf Mustavi <35128639+arafMustavi@users.noreply.github.com>
2020-06-15 13:52:57 +0600

    stripped through loops

--- momadataset.py
+++ momadataset.py
@@ -28,3 +28,31 @@ for entry in moma:
         
     entry[3] = clean_and_convert(entry[3])
     entry[4] = clean_and_convert(entry[4])
+    
+    
+    
+    
+    
+    
+ #Stripping Data#
+test_data = ["1912", "1929", "1913-1923",
+             "(1951)", "1994", "1934",
+             "c. 1915", "1995", "c. 1912",
+             "(1988)", "2002", "1957-1959",
+             "c. 1955.", "c. 1970's", 
+             "C. 1990-1999"]
+
+bad_chars = ["(",")","c","C",".","s","'", " "]
+
+def strip_characters(string):
+    for char in bad_chars:
+        string = string.replace(char,"")
+    return string
+
+cleaned_data = []
+for s in test_data:
+    s = strip_characters(s)
+    cleaned_data.append(s)
+print(cleaned_data)
+stripped_test_data = cleaned_data
+
